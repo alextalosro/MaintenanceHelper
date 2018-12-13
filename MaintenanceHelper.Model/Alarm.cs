2055c46bc73b915dc583ad6202d4faaad81dc4bb
Marin Alexandru Talos <alex.talos@windowslive.com>
2018-12-13 17:43:40 +0200

    Add project files.

---
+++ MaintenanceHelper.Model/Alarm.cs
@@ -0,0 +1,36 @@
+﻿namespace MaintenanceHelper.Model
+{
+    public class Alarm
+    {
+        public uint Index { get; set; }
+
+        public uint AlarmCode { get; set; }
+
+        public uint AlarmData { get; set; }
+
+        public uint AlarmType { get; set; }
+
+        public string AlarmTime { get; set; }
+
+        public string AlarmName { get; set; }
+
+        public string AlarmTask { get; set; }
+
+        public uint AlarmCodeSub { get; set; }
+
+        public uint AlarmDataSub { get; set; }
+
+        public uint AlarmTypeSub { get; set; }
+
+        public string AlarmTimeSub { get; set; }
+
+        public string AlarmNameSub { get; set; }
+
+        public string SubCodeDataAditionalInfo { get; set; }
+
+        public string SubCodeDataCharacterString { get; set; }
+
+        public string SubCodeDataCharacterStringRev { get; set; }
+
+    }
+}
